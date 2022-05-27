deb56a2e89d02172a16d13ea35eccaac45685715
mdAlnahian <muhammedalnahian@gmail.com>
2022-05-27 19:51:03 +0600

    everything updated

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -5,13 +5,21 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// };
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
+  apiKey: "AIzaSyDXKbmT8L2W8P0W3-3FmGvwwNy6vw1WSTQ",
+  authDomain: "big-bros-limited.firebaseapp.com",
+  projectId: "big-bros-limited",
+  storageBucket: "big-bros-limited.appspot.com",
+  messagingSenderId: "1041811293336",
+  appId: "1:1041811293336:web:4d19acdba85566aa653ced",
 };
 
 // Initialize Firebase
